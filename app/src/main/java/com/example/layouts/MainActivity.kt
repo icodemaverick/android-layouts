5e41e0327932710b0e8fdedfea51a04b68f4c964
Mario Meléndez <mmelendez@siget.gob.sv>
2025-02-11 16:59:44 -0600

    first commit

---
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.layouts
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnLinearLayout : Button = findViewById(R.id.BtnLinearLayout)
+        val btnConstraintLayout : Button = findViewById(R.id.BtnConstraintLayout)
+        val btnFrameLayout : Button = findViewById(R.id.BtnFrameLayout)
+        val btnTableLayout : Button = findViewById(R.id.BtnTableLayout)
+
+        btnLinearLayout.setOnClickListener {
+            val intent = Intent(this, LinearActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnConstraintLayout.setOnClickListener {
+            val intent = Intent(this, ConstraintActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnFrameLayout.setOnClickListener {
+
+        }
+
+        btnTableLayout.setOnClickListener {
+
+        }
+    }
+}
